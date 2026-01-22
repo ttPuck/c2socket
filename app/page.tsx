de800ff4affeb8eb819cc8d5082b08da744c2157
Twees <ptwees@icloud.com>
2026-01-21 19:10:29 -0500

    Scaffold 1
    Organizational folders setup for Next.js app routing
    
    Layout.tsx updated for sign-up and sign-in routes
    
    Main files (layout, css, and favicon) moved and organized into MAIN

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentExample } from "@/components/component-example";
-import { Button } from "@/components/ui/button"
-import { ArrowUpIcon } from "lucide-react"
-
-export default function Page() {
-return 
-}
